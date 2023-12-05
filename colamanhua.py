d13ee6753890740f093999e9e689fb341e4b1496
Simplxs <simplxsa@gmail.com>
2023-12-05 22:32:49 +0800

    fix KEY_TABLE

--- colamanhua.py
+++ colamanhua.py
@@ -19,7 +19,9 @@ def visit(url):
 
 
 def download(url, Referer):
-    return requests.get(url, headers={"Origin": DOMAIN, "Referer": Referer}, impersonate="chrome110")
+    return requests.get(
+        url, headers={"Origin": DOMAIN, "Referer": Referer}, impersonate="chrome110"
+    )
 
 
 def decrypt_ecb(data, key):
@@ -33,7 +35,7 @@ def decrypt_cbc(data, key):
 
 
 def decode_str(N):
-    data = aK[N - 0x1f0]
+    data = aK[N - 0x1F0]
     missing_padding = 4 - len(data) % 4
     if missing_padding != 4:
         data += "=" * missing_padding
@@ -55,27 +57,28 @@ def downloadImage(page_filename, page_url, Referer, key):
     file.write(decrypt_cbc(res.content, key))
 
 
+cal = lambda N: int(re.match(r"^\d+", decode_str(N).decode()).group())
+
 js = visit(f"{DOMAIN}/js/manga.read.js")
-aK = deque([i[1:-1]
-           for i in re.findall(r"var aK=\[(.*?)\];", js.text)[0].split(",")])
-table = re.findall(
-    r"G==ag\((0x[0-9a-f]*?)\)&&\(I=ag\((0x[0-9a-f]*?)\)\),", js.text)
+aK = deque([i[1:-1] for i in re.findall(r"var aK=\[(.*?)\];", js.text)[0].split(",")])
+table = re.findall(r"G==ag\((0x[0-9a-f]*?)\)&&\(I=ag\((0x[0-9a-f]*?)\)\),", js.text)
+condition, value = re.findall(r"var f=(.*?);.*?\(c,(0x[0-f]*)\)", js.text)[0]
 
-
-def cal(N): return int(re.search(r'\d+', decode_str(N).decode()).group())
-
+condition = re.sub(
+    r"parseInt\(N\((0x[0-f]*)\)\)", lambda N: f"cal({N.group(1)})", condition
+)
+value = int(value, 0)
 
 while True:
     try:
-        if -cal(0x928) / 0x1 + cal(0x62b) / 0x2 + -cal(0x699) / 0x3 * (cal(0x52a) / 0x4) + cal(0x5b0) / 0x5 + -cal(0xd85) / 0x6 * (cal(0x6e2) / 0x7) + -cal(0xab6) / 0x8 + cal(0xbdf) / 0x9 == 0xd2942:
+        if eval(condition) == value:
             break
         else:
             aK.rotate(-1)
     except:
         aK.rotate(-1)
 
-KEY_TABLE = {decode_str(int(k, 0)).decode(): decode_str(int(v, 0))
-             for k, v in table}
+KEY_TABLE = {decode_str(int(k, 0)).decode(): decode_str(int(v, 0)) for k, v in table}
 
 pool = ThreadPoolExecutor(max_workers=4)
 
@@ -88,10 +91,8 @@ def auto(id, cname):
     response = visit(f"{DOMAIN}/manga-{id}")
 
     myhtml = etree.HTML(response.text)
-    chapter_url_list = myhtml.xpath(
-        "//div[@class='all_data_list']/ul/li/a/@href")
-    chapter_name_list = myhtml.xpath(
-        "//div[@class='all_data_list']/ul/li/a/@title")
+    chapter_url_list = myhtml.xpath("//div[@class='all_data_list']/ul/li/a/@href")
+    chapter_name_list = myhtml.xpath("//div[@class='all_data_list']/ul/li/a/@title")
 
     task_list = []
 
@@ -121,8 +122,7 @@ def auto(id, cname):
             re.S,
         )
 
-        startimg, enc_code1, enc_code2, keyType, imgKey = pattern.findall(mh_info)[
-            0]
+        startimg, enc_code1, enc_code2, keyType, imgKey = pattern.findall(mh_info)[0]
 
         enc_code2 = base64.b64decode(base64.b64decode(enc_code2).decode())
         try:
@@ -134,7 +134,9 @@ def auto(id, cname):
             _tka_ = decrypt_ecb(
                 enc_code2,
                 b"0aW5swj3TBKSrfBU",
-            ).decode("gbk")  # _tka_ + mh_info["pageid"]
+            ).decode(
+                "gbk"
+            )  # _tka_ + mh_info["pageid"]
 
         enc_code1 = base64.b64decode(base64.b64decode(enc_code1).decode())
         try:
@@ -162,20 +164,34 @@ def auto(id, cname):
                 )
 
         for page in range(int(startimg), int(_tkb_) + 1):
-            if keyType != '0' and keyType != "":
+            if keyType != "0" and keyType != "":
                 task_list.append(
                     pool.submit(
-                        downloadImage, f".\\{page}.webp", f"{DOMAIN_IMG}/comic/{_tka_}{page:0>4d}.enc.webp", chapter_url, KEY_TABLE[keyType])
+                        downloadImage,
+                        f".\\{page}.webp",
+                        f"{DOMAIN_IMG}/comic/{_tka_}{page:0>4d}.enc.webp",
+                        chapter_url,
+                        KEY_TABLE[keyType],
+                    )
                 )
             elif imgKey != "":
                 task_list.append(
                     pool.submit(
-                        downloadImage, f".\\{page}.webp", f"{DOMAIN_IMG}/comic/{_tka_}{page:0>4d}.enc.webp", chapter_url, imgKey)
+                        downloadImage,
+                        f".\\{page}.webp",
+                        f"{DOMAIN_IMG}/comic/{_tka_}{page:0>4d}.enc.webp",
+                        chapter_url,
+                        imgKey,
+                    )
                 )
             else:
                 task_list.append(
                     pool.submit(
-                        downloadImage, f".\\{page}.jpg", f"{DOMAIN_IMG}/comic/{_tka_}{page:0>4d}.jpg", chapter_url)
+                        downloadImage,
+                        f".\\{page}.jpg",
+                        f"{DOMAIN_IMG}/comic/{_tka_}{page:0>4d}.jpg",
+                        chapter_url,
+                    )
                 )
             time.sleep(0.1)
         wait(task_list, return_when=ALL_COMPLETED)
